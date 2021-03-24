d071da35ec829c0ae96e2b938a87d1638e5686d6
tracerline <tracerline38@gmail.com>
2021-03-24 17:41:06 +0100

    commit

---
+++ src/app/all-musics/all-musics.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-all-musics',
+  templateUrl: './all-musics.component.html',
+  styleUrls: ['./all-musics.component.scss']
+})
+export class AllMusicsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SingleMusicComponent } from './single-music/single-music.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Navbar } from './navbar/navbar.component';
+import { AllMusicsComponent } from './all-musics/all-musics.component'
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingleMusicComponent
+    SingleMusicComponent,
+    Navbar,
+    AllMusicsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
